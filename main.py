db31db3333a4046434f8884ccf3f12c5408f9f8d
Yuki <risaton@risaton.net>
2023-01-21 01:55:11 +0900

    commit

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
-import kivy
+# -*- coding: utf-8 -*-
 
-from kivy import *
-from kivy.app import *
-from kivy.properties import StringProperty
-# from kivy.core.text import Label
-from kivy.uix import *
-from kivy.uix.boxlayout import *
-from kivy.uix.button import *
-from kivy.uix.label import *
+from kivy.app import App
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+from kivy.uix import label
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+# from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import *
+
+resource_add_path('fonts')
+LabelBase.register(DEFAULT_FONT, 'mplus-2c-regular.ttf')
 
 
-class BoxLayout1(BoxLayout):
+class Widget1(Widget):
+    # text = StringProperty()  # プロパティの追加
+    # lb1 =
+    def __init__(self, **kwargs):
+        super(Widget1, self).__init__(**kwargs)
+        # self.text = ''
 
-    def build(self, **kwargs):
-        button1 = Button
-        button1.text = 'aaaaa'
-
-    # def clicked1(self):
-    #     self.button1 = 'bbbb'
-    # button1.text = 'bbbb'
+    def clicked1(self):  # ボタンをクリック時
+        # self.lb1
+        # print(self.lb1)
+        # self.ids.label1.font_name = './fonts/Roboto-Medium.ttf'
+        self.ids.label1.text = 'こんにちは'
 
 
 class App1(App):
     def __init__(self, **kwargs):
         super(App1, self).__init__(**kwargs)
-        BoxLayout1()
+        # self.title = 'greeting'
+
+    def build(self):
+        return Widget1()
 
 
-    # def build(self):
-    #     return BoxLayout1().button1
-    # text = StringProperty()
-
-    # text = StringProperty()
-
-    # def build(self):
-    #     return Label(text='Hello, world!')
-
-
-App1().run()
+if __name__ == '__main__':
+    App1().run()
