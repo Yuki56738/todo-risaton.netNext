a439f7e6c95ca0566a0adaf4cf47389c298bf3a0
Yuki <risaton@risaton.net>
2023-01-20 22:30:31 +0900

    commit

--- main.py
+++ main.py
@@ -2,12 +2,39 @@ import kivy
 
 from kivy import *
 from kivy.app import *
+from kivy.properties import StringProperty
 # from kivy.core.text import Label
 from kivy.uix import *
+from kivy.uix.boxlayout import *
+from kivy.uix.button import *
 from kivy.uix.label import *
 
 
+class BoxLayout1(BoxLayout):
+
+    def build(self, **kwargs):
+        button1 = Button
+        button1.text = 'aaaaa'
+
+    # def clicked1(self):
+    #     self.button1 = 'bbbb'
+    # button1.text = 'bbbb'
+
+
 class App1(App):
-    def build(self):
-        return Label(text='Hello, world!')
+    def __init__(self, **kwargs):
+        super(App1, self).__init__(**kwargs)
+        BoxLayout1()
+
+
+    # def build(self):
+    #     return BoxLayout1().button1
+    # text = StringProperty()
+
+    # text = StringProperty()
+
+    # def build(self):
+    #     return Label(text='Hello, world!')
+
+
 App1().run()
