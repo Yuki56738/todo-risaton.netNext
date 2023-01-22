46aa04920c891f9f8f885cf59477bb2f6b4ae7c1
Yuki <risaton@risaton.net>
2023-01-22 14:50:15 +0900

    commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import kivy
+from kivy.core.text import *
+from kivy.core.text import DEFAULT_FONT
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -6,11 +9,24 @@ from datetime import datetime
 from kivymd.uix.list import TwoLineAvatarIconListItem, ILeftBodyTouch
 from kivymd.uix.selectioncontrol import MDCheckbox
 from kivy.utils import platform
-
+# import japanize_kivy
+from kivy.core.text import *
+from kivy.resources import *
+resource_add_path('fonts/mplus-2c-regular.ttf')
+LabelBase.register(DEFAULT_FONT, 'fonts/mplus-2c-regular.ttf')
+# FontContextManager.create('myfont')
+# family = FontContextManager.add_font('fonts/mplus-2c-regular.ttf')
+# FontContextManager.add_font('fonts')
+# FontContextManager.add_font(filename='fonts/mplus-2c-regular.ttf')
+# FontContextManagerBase.add_font('fonts/mplus-2c-regular.ttf')
+# DEFAULT_FONT = 'fonts/mplus-2c-regular.ttf'
+# LabelBase.register(DEFAULT_FONT, resource_path())
 # if platform == "android":
 #     from android.permissions import request_permissions, Permission
 #     request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+# DEFAULT_FONT.font_name = 'myfont'
 
+# DEFAULT_FONT = 'fonts/mplus-2c-regular.ttf'
 
 from database import Database
 
@@ -20,11 +36,11 @@ db = Database()
 
 class DialogContent(MDBoxLayout):
     """OPENS A DIALOG BOX THAT GETS THE TASK FROM THE USER"""
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ids.date_text.text = str(datetime.now().strftime('%A %d %B %Y'))
 
-    
     def show_date_picker(self):
         """Opens the date picker"""
         date_dialog = MDDatePicker()
@@ -47,29 +63,27 @@ class ListItemWithCheckbox(TwoLineAvatarIconListItem):
         # state a pk which we shall use link the list items with the database primary keys
         self.pk = pk
 
-
     def mark(self, check, the_list_item):
         '''mark the task as complete or incomplete'''
         if check.active == True:
-            the_list_item.text = '[s]'+the_list_item.text+'[/s]'
-            db.mark_task_as_complete(the_list_item.pk)# here
+            the_list_item.text = '[s]' + the_list_item.text + '[/s]'
+            db.mark_task_as_complete(the_list_item.pk)  # here
         else:
-            the_list_item.text = str(db.mark_task_as_incomplete(the_list_item.pk))# Here
+            the_list_item.text = str(db.mark_task_as_incomplete(the_list_item.pk))  # Here
 
     def delete_item(self, the_list_item):
         '''Delete the task'''
         self.parent.remove_widget(the_list_item)
-        db.delete_task(the_list_item.pk)# Here
-
+        db.delete_task(the_list_item.pk)  # Here
 
 
 class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
     '''Custom left container'''
 
 
-
 class MainApp(MDApp):
     task_list_dialog = None
+
     def build(self):
         # Setting theme to my favorite theme
         self.theme_cls.primary_palette = "DeepPurple"
@@ -77,7 +91,8 @@ class MainApp(MDApp):
     def show_task_dialog(self):
         if not self.task_list_dialog:
             self.task_list_dialog = MDDialog(
-                title="Create Task",
+                title="タスクを作成する",
+                # font_name = 'fonts/mplus-2c-regular.ttf',
                 type="custom",
                 content_cls=DialogContent(),
             )
@@ -85,18 +100,20 @@ class MainApp(MDApp):
         self.task_list_dialog.open()
 
     def on_start(self):
+
+        # LabelBase.register('myfont', 'fonts/mplus-2c-regular.ttf', 'fonts/mplus-2c-regular.ttf', 'fonts/mplus-2c-regular.ttf', 'fonts/mplus-2c-regular.ttf')
         """Load the saved tasks and add them to the MDList widget when the application starts"""
         try:
             completed_tasks, uncomplete_tasks = db.get_tasks()
 
             if uncomplete_tasks != []:
                 for task in uncomplete_tasks:
-                    add_task = ListItemWithCheckbox(pk=task[0],text=task[1], secondary_text=task[2])
+                    add_task = ListItemWithCheckbox(pk=task[0], text=task[1], secondary_text=task[2])
                     self.root.ids.container.add_widget(add_task)
 
             if completed_tasks != []:
                 for task in completed_tasks:
-                    add_task = ListItemWithCheckbox(pk=task[0],text='[s]'+task[1]+'[/s]', secondary_text=task[2])
+                    add_task = ListItemWithCheckbox(pk=task[0], text='[s]' + task[1] + '[/s]', secondary_text=task[2])
                     add_task.ids.check.active = True
                     self.root.ids.container.add_widget(add_task)
         except Exception as e:
@@ -108,13 +125,18 @@ class MainApp(MDApp):
 
     def add_task(self, task, task_date):
         '''Add task to the list of tasks'''
+        # resource_add_path('fonts/mplus-2c-regular.ttf')
+        # LabelBase.register(DEFAULT_FONT, 'fonts/mplus-2c-regular.ttf')
 
         # Add task to the db
-        created_task = db.create_task(task.text, task_date)# Here
+        created_task = db.create_task(task.text, task_date)  # Here
 
         # return the created task details and create a list item
-        self.root.ids['container'].add_widget(ListItemWithCheckbox(pk=created_task[0], text='[b]'+created_task[1]+'[/b]', secondary_text=created_task[2]))# Here
+        self.root.ids['container'].add_widget(
+            ListItemWithCheckbox(pk=created_task[0], text='[b]' + created_task[1] + '[/b]',
+                                 secondary_text=created_task[2]))  # Here
         task.text = ''
+
 
 if __name__ == '__main__':
     app = MainApp()
