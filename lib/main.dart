9f3ced5013b1e46f1edc255f076951721c9cc6e4
Yuki <risaton@risaton.net>
2023-04-07 22:53:33 +0900

    added delete button and storage future

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
+final storage = const FlutterSecureStorage();
+List todolist = [];
+
 void main() {
+  // Map<String, String> allValues = await storage.readAll();
+  // debugPrint(allValues.toString());
   runApp(MyTodoApp());
 }
 
 class MyTodoApp extends StatelessWidget {
+  // List todolist = [];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,10 +36,24 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  List todolist = [];
+  _readAll() async {
+    Map<String, String> allValues = await storage.readAll();
+    debugPrint(allValues.toString());
+    List fromStorageAllList = [];
+    allValues.forEach((key, value) {
+      fromStorageAllList.add(value);
+    });
+    setState(() {
+      todolist.addAll(fromStorageAllList);
+    });
+  }
+
   @override
   void initState() {
-    super.initState();
+    _readAll();
+    // super.initState();
+    // todolist.add(getAll());
+
     // _initializePlatformSpecifics();
     // _showNotification();
   }
@@ -58,30 +79,29 @@ class _TodoListPageState extends State<TodoListPage> {
                   );
                 } else if (index == todolist.length) {
                   // return Container(
-                  return Column(children: <Widget>[
-                    ElevatedButton(
-                        onPressed: () {
-                          // todolist.clear();
-                          setState(() {
-                            todolist.clear();
-                          });
-                        },
-                        child: const Text('リストを全消去',
-                            style: TextStyle(
-                                fontSize: 36,
-                                color: Colors.white,
-                                backgroundColor: Colors.blue))),
-                    ElevatedButton(
-                        onPressed: () {
-                          _initializePlatformSpecifics();
-                          _showNotification();
-                        },
-                        child: const Text('通知のテスト',
-                            style: TextStyle(
-                                fontSize: 26,
-                                color: Colors.white,
-                                backgroundColor: Colors.blue)))
-                  ]);
+                  // return Column(children: <Widget>[
+                  //   ElevatedButton(
+                  //       onPressed: () {
+                  //         // todolist.clear();
+                  //         setState(() {
+                  //           todolist.clear();
+                  //         });
+                  //       },
+                  //       child: const Text('リストを全消去',
+                  //           style: TextStyle(
+                  //               fontSize: 36,
+                  //               color: Colors.white,
+                  //               backgroundColor: Colors.blue))),
+                  return ElevatedButton(
+                      onPressed: () {
+                        _initializePlatformSpecifics();
+                        _showNotification();
+                      },
+                      child: const Text('通知のテスト',
+                          style: TextStyle(
+                              fontSize: 26,
+                              color: Colors.white,
+                              backgroundColor: Colors.blue)));
                 }
               })
         ],
@@ -95,6 +115,11 @@ class _TodoListPageState extends State<TodoListPage> {
           if (newListText != null) {
             setState(() {
               todolist.add(newListText);
+            });
+          }
+          if(newListText == null){
+            setState(() {
+              todolist.clear();
             });
           }
         },
@@ -135,10 +160,26 @@ class _TodoAddPageState extends State<TodoAddPage> {
               Container(
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(primary: Colors.red),
-                  onPressed: () {
+                  onPressed: () async {
+                    // final storage = FlutterSecureStorage();
+                    Map<String, String> allValues = await storage.readAll();
+                    debugPrint(allValues.toString());
+                    int currentValue = allValues.length + 1;
+                    await storage.write(
+                        key: currentValue.toString(), value: _text);
                     Navigator.of(context).pop(_text);
                   },
                   child: Text('追加する。'),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(onPressed: () {
+                  todolist.clear();
+                  storage.deleteAll();
+                  Navigator.of(context).pop(null);
+
+                }, child: Text("全て削除する"),
+
                 ),
               )
             ],
@@ -152,9 +193,12 @@ void _initializePlatformSpecifics() {
       requestAlertPermission: true,
       requestBadgePermission: true,
       requestSoundPermission: true);
-  var initializationSettingsAndroid = const AndroidInitializationSettings("@mipmap/ic_launcher");
+  var initializationSettingsAndroid =
+      const AndroidInitializationSettings("@mipmap/ic_launcher");
   var initializationSettings = InitializationSettings(
-      iOS: initializationSettingsIOS, macOS: initializationSettingsIOS, android: initializationSettingsAndroid);
+      iOS: initializationSettingsIOS,
+      macOS: initializationSettingsIOS,
+      android: initializationSettingsAndroid);
   flutterLocalNotificationsPlugin.initialize(initializationSettings,
       onDidReceiveNotificationResponse: (NotificationResponse res) {
     debugPrint('payload:${res.payload}');
@@ -165,8 +209,10 @@ Future<void> _showNotification() async {
   var iosChannelSpecifics = DarwinNotificationDetails();
   var androidChannelSpecifics = AndroidNotificationDetails("0", "risaton.net");
 
-  var platformChannelSpecifics =
-      NotificationDetails(iOS: iosChannelSpecifics, macOS: iosChannelSpecifics, android: androidChannelSpecifics);
+  var platformChannelSpecifics = NotificationDetails(
+      iOS: iosChannelSpecifics,
+      macOS: iosChannelSpecifics,
+      android: androidChannelSpecifics);
 
   await flutterLocalNotificationsPlugin.show(
     0, // Notification ID
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import flutter_app_badger
 import flutter_local_notifications
 import flutter_native_timezone
+import flutter_secure_storage_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
 }
